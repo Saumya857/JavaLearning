a0b9a1b2ac2bc4ae20561c454c15faaac21a840a
Saumya Singh <saumya0309singh@gmail.com>
2024-05-17 00:22:31 +0530

    First Exercise

--- Ex01.java
+++ Ex01.java
@@ -1,14 +1,5 @@
-public class Ex01
-{
-    public static void main(String []args)
-    {
-        List<Integer> number = List.of(12,5,3,2,10,7,89,62);
-    }
-
-    private static void printOddNumbers(List<Integer> numbers)
-    {
-        numbers.stream()
-        .filter(number -> number%2!=0)
-        .forEach(System.out :: println)
+public class Ex01 {
+    public static void main(String []args) {
+        System.out.println("Hello World!");
     }
 }
