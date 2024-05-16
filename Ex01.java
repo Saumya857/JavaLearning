8a52d6421cf3c8c56240f12d7f098944bdbcea0f
Saumya Singh <saumya0309singh@gmail.com>
2024-05-17 00:32:56 +0530

    Printing Odd Numbers Functionally

--- Ex01.java
+++ Ex01.java
@@ -1,5 +1,15 @@
+import java.util.List;
+
 public class Ex01 {
     public static void main(String []args) {
-        System.out.println("Hello World!");
+        List<Integer> number = List.of(121,3,56,77,44,10,21,9);
+        printOddNumbers(number);
+    }
+
+    private static void printOddNumbers(List<Integer> numbers)
+    {
+        numbers.stream()
+        .filter(number -> number%2!=0)
+        .forEach(System.out :: println);
     }
 }
